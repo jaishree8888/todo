5b2a14d53879ae8a5518dc1591b49a10cb70db9e
jaishree8888 <jaishreeyuva88@gmail.com>
2025-07-08 11:15:12 +0530

    DB Connected

--- src/main/java/com/example/todo/model/Task.java
+++ src/main/java/com/example/todo/model/Task.java
@@ -1,6 +1,20 @@
 package com.example.todo.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Table(name="tasks")
 public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String title;
     private String description;
